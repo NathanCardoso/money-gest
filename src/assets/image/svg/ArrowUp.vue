aab37f850a276522064d5950d4a4a5495bbb35f5
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-09-25 20:02:57 -0300

    feat[color]: to add new color for bodyApp and to add new component svg arrowUp

---
+++ src/assets/image/svg/ArrowUp.vue
@@ -0,0 +1,24 @@
+<template>
+  <svg
+    width="12"
+    height="16"
+    viewBox="0 0 12 16"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M0.99991 5.6665L5.56221 0.999838L10.1245 5.6665"
+      stroke="black"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+    <path
+      d="M5.56225 1L5.56226 15"
+      stroke="black"
+      stroke-width="2"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+</template>

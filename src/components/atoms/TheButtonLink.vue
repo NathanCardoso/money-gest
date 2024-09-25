1299bfc4024780c2bba63c2664ae7aa953bea302
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-09-25 20:03:53 -0300

    feat[components/atoms]: create atom TheButtonLink

---
+++ src/components/atoms/TheButtonLink.vue
@@ -0,0 +1,39 @@
+<template>
+  <button class="button-link" @click="handleClick">
+    <slot></slot>
+    {{ buttonMessage }}
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "theButtonLink",
+
+  props: {
+    buttonMessage: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    handleClick() {
+     this.$emit("the-button:click")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.button-link {
+  padding: rem(6) rem(12);
+  background: $white;
+  border: none;
+  border-radius: rem(6);
+  color: $black;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: rem(8);
+}
+</style>

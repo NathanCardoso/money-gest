f1d92ebe7eb6a89d4967c0adfb7910f836dc278f
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-09-25 21:51:16 -0300

    feat[props]: to add new prop 'iconColor' and dynamic color for stroke svg

--- src/assets/image/svg/IconArrowUp.vue
+++ src/assets/image/svg/IconArrowUp.vue
@@ -8,17 +8,41 @@
   >
     <path
       d="M0.99991 5.6665L5.56221 0.999838L10.1245 5.6665"
-      stroke="black"
+      :stroke="strokeColor"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
     <path
       d="M5.56225 1L5.56226 15"
-      stroke="black"
+      :stroke="strokeColor"
       stroke-width="2"
       stroke-linecap="round"
       stroke-linejoin="round"
     />
   </svg>
 </template>
+
+<script lang="ts">
+import { useStrokeColor } from '~/composables/useStrokeColor';
+
+export default {
+  name: "IconArrowUp",
+
+  props: {
+    iconColor: {
+      type: String,
+      default: "#000000"
+    }
+  },
+
+  setup(props) {
+    const { iconColor } = toRefs(props)
+    const { strokeColor } = useStrokeColor(iconColor)
+
+    return {
+      strokeColor
+    }
+  }
+}
+</script>

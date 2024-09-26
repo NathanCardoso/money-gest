4fc628efaddacf3c8df8baefe489da348c174b3a
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-09-25 21:53:29 -0300

    feat[componnets/molecules]: create new molecule 'TheCardInfoMoney'

---
+++ src/components/molecules/TheCardInfoMoney.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="card-info-money">
+    <div class="icon-wrapper">
+      <slot class=""></slot>
+    </div>
+    <div class="info-wrapper">
+      <TheTitleMoney :title-message="moneyValue" :title-color="moneyColor" />
+      <TheParagraph :paragraph-message="titleCard" />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TheCardInfoMoney",
+
+  props: {
+    titleCard: {
+      type: String,
+      required: true
+    },
+    moneyValue: {
+      type: String,
+      required: true
+    },
+    moneyColor: {
+      type: String,
+      required: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.card-info-money {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  background: $white;
+  border-radius: rem(12);
+  padding: rem(20) rem(32);
+
+  .icon-wrapper {
+    width: rem(50);
+    height: rem(50);
+    border-radius: 50%;
+    background: $greenLight;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+
+  .info-wrapper {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    gap: rem(16);
+  }
+}
+</style>

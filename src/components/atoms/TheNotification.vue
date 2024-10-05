cc4f29b8ad94538d5f85577213daa37eec366295
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-05 16:52:20 -0300

    feat[components/atoms]: crete new component 'TheNotification'

---
+++ src/components/atoms/TheNotification.vue
@@ -0,0 +1,30 @@
+<template>
+  <button class="notification">
+    <IconNotification />
+  </button>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TheIconNotification",
+
+  methods: {
+    handleClick(): void {
+      this.$emit("notification:click")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.notification {
+  background: $black;
+  border: none;
+  border-radius: rem(4);
+  width: rem(40);
+  height: rem(40);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+</style>

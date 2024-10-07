020e7cab078864118739e85dd167d2676be96ed3
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-07 20:31:01 -0300

    feat[components/organisms]: create new organisms 'TheHeader'

---
+++ src/components/organisms/TheHeader.vue
@@ -0,0 +1,65 @@
+<template>
+  <header class="header">
+    <div class="header-wrapper container">
+      <div class="header-navigation">
+        <IconLogo />
+        <TheHeaderNavigation :routes="routes" />
+      </div>
+      <div class="header-user">
+        <TheUserProfile user-name="Nathan" user-email="nathan@gmail.com" />
+        <TheNotification />
+      </div>
+    </div>
+  </header>
+</template>
+
+<script lang="ts">
+import type { IHeaderNavigationProp } from "../../interface/molecules/TheHeaderNavigation"
+
+export default {
+  name: "TheHeader",
+
+  data() {
+    return {
+      routes: [
+        {
+          id: 1,
+          name: "Entrada",
+          path: "/entrada"
+        },
+        {
+          id: 2,
+          name: "saída",
+          path: "/saida"
+        },
+        {
+          id: 3,
+          name: "Categoria",
+          path: "/categoria"
+        }
+      ] as IHeaderNavigationProp[]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.header {
+  background: $white;
+  padding: rem(24) 0;
+
+  .header-wrapper {
+    @include useAlignBetween;
+
+    .header-navigation {
+      @include useAlignBetween;
+      gap: rem(24);
+    }
+
+    .header-user {
+      @include useAlignBetween;
+      gap: rem(10);
+    }
+  }
+}
+</style>

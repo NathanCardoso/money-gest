3ac8e381e19ea9e3b3014c05bd086434ef1c84ae
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-07 23:47:46 -0300

    feat[layouts]: to add new classes

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <TheHeader />
-    <slot></slot>
+    <div class="content">
+      <div class="container">
+        <slot></slot>
+      </div>
+    </div>
     <TheFooter />
   </div>
 </template>
+
+<style lang="scss" scoped>
+.content {
+  padding: 0 rem(16);
+  margin: 0 auto;
+}
+</style>

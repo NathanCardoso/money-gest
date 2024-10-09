fa6ef246077926650f7b25bae930c745dfc7af27
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-09 19:53:43 -0300

    fix[components/organisms]: adjustment in box shadow

--- src/components/organisms/TheHeader.vue
+++ src/components/organisms/TheHeader.vue
@@ -54,7 +54,7 @@ export default {
     border-radius: rem(8);
     background: $white;
     padding: rem(16) 0;
-    box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+    box-shadow: 0px 2px 2px rgba(0, 0, 0, 0.25);
 
     .header-wrapper {
       @include useAlignCenterBetween;

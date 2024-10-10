995959087eefb775e5cd44a8fae3416380f4b6a5
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-09 21:38:42 -0300

    feat[components/organisms]: to add new component 'TheListAccount'

---
+++ src/components/organisms/TheListAccount.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="account-list">
+    <TheItemListAccount
+      v-for="(account, accountIndex) in accountList"
+      :key="accountIndex"
+      :item-account="account"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import type { IItemListAccountProp } from "~/interface/organisms/TheItemListAccount"
+
+export default {
+  name: "TheListAccount.vue",
+
+  props: {
+    accountList: {
+      type: Array as () => IItemListAccountProp[],
+      default: () => [] as IItemListAccountProp[]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.account-list {
+  width: 100%;
+}
+</style>

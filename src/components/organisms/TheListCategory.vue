c075cb284969ff15c019dc6dcfae92a7387aab0e
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-09 22:49:36 -0300

    feat[components/organisms]: create new component 'TheListCategory'

---
+++ src/components/organisms/TheListCategory.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul class="category-list">
+    <TheItemListCategory
+      v-for="(category, categoryIndex) in categoryList"
+      :key="categoryIndex"
+      :item-category="category"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import type { IItemListCategoryProp } from "~/interface/organisms/TheItemListCategory"
+
+export default {
+  name: "TheListCategory",
+
+  props: {
+    categoryList: {
+      type: Array as () => IItemListCategoryProp[],
+      default: () => [] as IItemListCategoryProp[]
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.category-list {
+  width: 100%;
+}
+</style>

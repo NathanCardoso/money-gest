d8671b79a8e6838e5700c759b97858d61956e723
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-12 14:23:10 -0300

    feat[components/organisms]: create new component 'TheListTransaction'

---
+++ src/components/organisms/TheListTransaction.vue
@@ -0,0 +1,35 @@
+<template>
+  <ul class="transaction-list">
+    <TheItemListTransaction
+      v-for="(transaction, transactionIndex) in transactionList"
+      :key="transactionIndex"
+      :item-transaction="transaction"
+      :is-popover="isPopover"
+    />
+  </ul>
+</template>
+
+<script lang="ts">
+import type { IItemListTransactionProp } from "~/interface/organisms/TheItemListTransaction"
+
+export default {
+  name: "TheListTransaction",
+
+  props: {
+    transactionList: {
+      type: Array as () => IItemListTransactionProp[],
+      default: () => [] as IItemListTransactionProp[]
+    },
+    isPopover: {
+      type: Boolean,
+      default: true
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.transaction-list {
+  width: 100%;
+}
+</style>

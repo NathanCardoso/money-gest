7e9f979f06f0611ba316c0865af27f1b4a057f39
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-15 21:11:46 -0300

    feat[components/organisms]: create new component 'TheModalFormCategory'

---
+++ src/components/organisms/TheModalFormCategory.vue
@@ -0,0 +1,75 @@
+<template>
+  <TheModal
+    :is-opened="isOpened"
+    title="Adicione uma nova categoria"
+    @modal:close="handleClose"
+    @modal:submit="handleSubmit"
+  >
+    <form class="form">
+      <TheInputField
+        is-label
+        input-type="text"
+        input-id="name"
+        input-name="Nome"
+        input-placeholder="Digite o nome da categoria..."
+      />
+      <TheSelect
+        is-label
+        select-name="Selecione a cor da categoria"
+        select-id="category"
+        :select-options="selectOptions"
+      />
+    </form>
+  </TheModal>
+</template>
+
+<script lang="ts">
+import type { ISelectOptionsProp } from "~/interface/atoms/TheSelect"
+
+export default {
+  name: "TheModalFormCategory",
+
+  props: {
+    isOpened: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      selectOptions: [
+        {
+          value: "",
+          label: "Selecione uma opção"
+        },
+        {
+          value: "M",
+          label: "Masculino"
+        },
+        {
+          value: "F",
+          label: "Feminino"
+        }
+      ] as ISelectOptionsProp[]
+    }
+  },
+
+  methods: {
+    handleClose() {
+      this.$emit("modal-category:close")
+    },
+    handleSubmit() {
+      this.$emit("modal-category:submit")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form {
+  @include useAlignStartCenter;
+  flex-direction: column;
+  gap: rem(16);
+}
+</style>

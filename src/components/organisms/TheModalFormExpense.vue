202722fc478e864d2d03d1f1c4f11d4f8b4c5732
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-15 21:12:09 -0300

    feat[components/organisms]: create new component 'TheModalFormExpense'

---
+++ src/components/organisms/TheModalFormExpense.vue
@@ -0,0 +1,88 @@
+<template>
+  <TheModal
+    :is-opened="isOpened"
+    title="Adicione uma nova saída"
+    @modal:close="handleClose"
+    @modal:submit="handleSubmit"
+  >
+    <form class="form">
+      <TheInputField
+        is-label
+        input-type="text"
+        input-id="name"
+        input-name="Nome"
+        input-placeholder="Digite o nome da entrada..."
+      />
+      <TheInputField
+        is-label
+        input-type="number"
+        input-id="revenue"
+        input-name="Valor"
+        input-placeholder="Digite o valor da entrada..."
+      />
+      <TheSelect
+        is-label
+        select-name="Conta"
+        select-id="account"
+        :select-options="selectOptions"
+      />
+      <TheSelect
+        is-label
+        select-name="Categoria"
+        select-id="category"
+        :select-options="selectOptions"
+      />
+    </form>
+  </TheModal>
+</template>
+
+<script lang="ts">
+import type { ISelectOptionsProp } from "~/interface/atoms/TheSelect"
+
+export default {
+  name: "TheModalFormExpense",
+
+  props: {
+    isOpened: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  data() {
+    return {
+      selectOptions: [
+        {
+          value: "",
+          label: "Selecione uma opção"
+        },
+        {
+          value: "M",
+          label: "Masculino"
+        },
+        {
+          value: "F",
+          label: "Feminino"
+        }
+      ] as ISelectOptionsProp[]
+    }
+  },
+
+  methods: {
+    handleClose() {
+      this.$emit("modal-expense:close")
+    },
+    handleSubmit() {
+      this.$emit("modal-expense:submit")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form {
+  @include useAlignStartCenter;
+  flex-direction: column;
+  gap: rem(16);
+}
+</style>

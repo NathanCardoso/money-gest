bcb384b396322b51665cd7ce040e742b6b7dcf71
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-17 22:01:55 -0300

    feat[compoennts/organisms]: create new component 'TheModalConfirmation'

---
+++ src/components/organisms/TheModalConfirmation.vue
@@ -0,0 +1,115 @@
+<template>
+  <transition name="modal-content">
+    <div v-if="isOpened" class="modal-overlay">
+      <div class="modal-content">
+        <TheTitlePage :title-message="titleConfirmation" class="modal-title" />
+        <TheParagraph :paragraph-message="paragraphConfirmation" />
+        <div class="modal-buttons">
+          <TheButtonForm button-message="Salvar" is-button-small />
+          <TheButtonForm
+            button-message="Cancelar"
+            is-button-small
+            button-cancel
+            @button:click="handleClose"
+          />
+        </div>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TheModalFormConfirmation",
+
+  props: {
+    isOpened: {
+      type: Boolean,
+      default: false
+    },
+    titleConfirmation: {
+      type: String,
+      required: true
+    },
+    paragraphConfirmation: {
+      type: String,
+      required: true
+    }
+  },
+
+  methods: {
+    handleClose() {
+      this.$emit("modal-confirmation:close")
+    },
+    handleSubmit() {
+      this.$emit("modal-confirmation:submit")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: rgba(0, 0, 0, 0.5);
+  @include useAlignCenter;
+  z-index: 1;
+
+  .modal-content {
+    width: rem(600);
+    background: $white;
+    padding: rem(32);
+    border-radius: rem(8);
+    position: relative;
+
+    .modal-title {
+      margin-bottom: rem(20);
+    }
+
+    .modal-close {
+      position: absolute;
+      top: rem(16);
+      right: rem(16);
+    }
+
+    .modal-buttons {
+      margin-top: rem(20);
+      @include useAlignCenterEnd;
+      gap: rem(16);
+    }
+  }
+}
+
+.modal-content-enter-from {
+  transform: translate3d(0, rem(-20), 0);
+  filter: blur(2px);
+  opacity: 0;
+}
+
+.modal-content-enter-to {
+  transform: translate3d(0, 0, 0);
+  filter: blur(0);
+  opacity: 1;
+}
+
+.modal-content-leave-from {
+  transform: translate3d(0, 0, 0);
+  opacity: 1;
+  filter: blur(0);
+}
+
+.modal-content-leave-to {
+  transform: translate3d(0, rem(-20), 0);
+  opacity: 0;
+  filter: blur(2px);
+}
+
+.modal-content-enter-active,
+.modal-content-leave-active {
+  transition: transform 0.3s ease, opacity 0.3s ease, filter 0.3s ease;
+}
+</style>

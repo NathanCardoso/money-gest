56a19fc8766b4e0ceee67a0c0098e7003c7c2bfc
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-17 22:13:14 -0300

    feat[components/organisms]: create new modals edit data foreach page

---
+++ src/components/organisms/TheModalEditCard.vue
@@ -0,0 +1,73 @@
+<template>
+  <TheModal
+    :is-opened="isOpened"
+    title="Edite o cartão"
+    @modal:close="handleClose"
+    @modal:submit="handleSubmit"
+  >
+    <form class="form">
+      <TheInputField
+        is-label
+        input-type="text"
+        input-id="name"
+        input-name="Nome do banco"
+        input-placeholder="Digite o nome do banco..."
+      />
+      <TheInputField
+        is-label
+        input-type="text"
+        input-id="name"
+        input-name="Nome do cartão"
+        input-placeholder="Digite o nome do cartão..."
+      />
+      <TheInputField
+        is-label
+        input-type="text"
+        input-id="name"
+        input-name="Limite do cartão"
+        input-placeholder="Digite o limite do cartão..."
+      />
+      <TheInputField
+        is-label
+        input-type="text"
+        input-id="name"
+        input-name="Números do Cartão"
+        input-placeholder="Digite o os números do cartão..."
+      />
+    </form>
+  </TheModal>
+</template>
+
+<script lang="ts">
+export default {
+  name: "TheModalEditCard",
+
+  props: {
+    isOpened: {
+      type: Boolean,
+      default: false
+    },
+    cardData: {
+      type: Object,
+      default: () => {}
+    }
+  },
+
+  methods: {
+    handleClose() {
+      this.$emit("modal-card:close")
+    },
+    handleSubmit() {
+      this.$emit("modal-card:submit")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form {
+  @include useAlignStartCenter;
+  flex-direction: column;
+  gap: rem(16);
+}
+</style>
---
+++ src/components/organisms/TheModalEditCategory.vue
@@ -0,0 +1,79 @@
+<template>
+  <TheModal
+    :is-opened="isOpened"
+    title="Edite a categoria"
+    @modal:close="handleClose"
+    @modal:submit="handleSubmit"
+  >
+    <form class="form">
+      <TheInputField
+        is-label
+        input-type="text"
+        input-id="name"
+        input-name="Nome"
+        input-placeholder="Digite o nome da categoria..."
+      />
+      <TheSelect
+        is-label
+        select-name="Selecione a cor da categoria"
+        select-id="category"
+        :select-options="selectOptions"
+      />
+    </form>
+  </TheModal>
+</template>
+
+<script lang="ts">
+import type { ISelectOptionsProp } from "~/interface/atoms/TheSelect"
+
+export default {
+  name: "TheModalEditCategory",
+
+  props: {
+    isOpened: {
+      type: Boolean,
+      default: false
+    },
+    categoryData: {
+      type: Object,
+      default: () => {}
+    }
+  },
+
+  data() {
+    return {
+      selectOptions: [
+        {
+          value: "",
+          label: "Selecione uma opção"
+        },
+        {
+          value: "M",
+          label: "Masculino"
+        },
+        {
+          value: "F",
+          label: "Feminino"
+        }
+      ] as ISelectOptionsProp[]
+    }
+  },
+
+  methods: {
+    handleClose() {
+      this.$emit("modal-category:close")
+    },
+    handleSubmit() {
+      this.$emit("modal-category:submit")
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.form {
+  @include useAlignStartCenter;
+  flex-direction: column;
+  gap: rem(16);
+}
+</style>

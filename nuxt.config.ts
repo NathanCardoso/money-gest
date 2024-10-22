f6af67d7336987ad967cf89bad6ee2825e2f8f3b
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-21 21:59:11 -0300

    feat[nuxt.config]: to add new plugin pinia

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,30 +3,40 @@ export default defineNuxtConfig({
   app: {
     pageTransition: { name: 'page', mode: 'out-in' }
   },
+
   srcDir: 'src/',
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
+
+  plugins: [
+    '~/plugins/pinia.ts',
+  ],
+
   components: [
     { path: './components/atoms/' },
     { path: './components/molecules/' },
     { path: './components/organisms/' },
     { path: './assets/image/svg' }
   ],
+
   css: [
     '~/assets/scss/common/reset.scss'
   ],
+
   sourcemap: {
     server: false,
     client: false,
   },
+
   vite: {
     css: {
       preprocessorOptions: {
         scss: {
           api: 'modern-compiler',
-          additionalData: '@use "~/assets/scss/main.scss" as *;'
+          additionalData: '@use "~/assets/scss/main.scss" as *;',
+          quietDeps: true
         }
       }
     }
-  },
+  }
 })

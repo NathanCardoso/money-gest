84de75f9f737b256a4ae9c71d02dd7f2ad4f1f42
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-10-21 22:07:25 -0300

    fix: [store]: replace name file

--- src/store/useTransactionRevenue.ts
+++ src/store/useTransactionRevenue.ts
@@ -3,7 +3,7 @@ import transactionRevenueService from '~/services/useServiceTransactionRevenue'
 import type { IItemListTransactionProp } from '~/interface/organisms/TheItemListTransaction';
 import type { IModalCreateOrEditRevenueData } from '~/interface/organisms/TheModalCreateOrEditRevenue';
 
-export const useTransactionStore = defineStore('transaction', {
+export const useStoreRevenue = defineStore('transaction', {
   state: () => ({
     transactions: [] as IItemListTransactionProp[],
   }),

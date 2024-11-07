3bfe8d28ce1108b8a371249b06f141a5f790c34b
Nathan Cardoso <82064891+NathanCardoso@users.noreply.github.com>
2024-11-06 21:15:33 -0300

    feat[interface/page]: to add new interface

--- src/interface/pages/user.ts
+++ src/interface/pages/user.ts
@@ -1,10 +1,23 @@
 export interface IUserRegister {
   name: string;
   email: string;
+  age: number;
   dateBirthday: string;
   gender: string;
   password: string;
   confirmPassword: string;
+}
+
+export interface IUserLogin {
+  email: string;
+  password: string;
+}
+
+export interface IUserLoginResponse {
+  token: string;
+  id: string;
+  email: string;
+  name: string;
 }
 
 export interface IUserData {
